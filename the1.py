b68f5fad01d17e9c1aaebcbd094cde9e82bc94d6
mohamedmhmuod5555-design <mohamedmhmuod5555@gmail.com>
2026-07-25 16:49:28 -0700

    Update the1.py

--- the1.py
+++ the1.py
@@ -2,7 +2,7 @@ import random
 import streamlit as st 
 if 'num' not in st.session_state:
   st.session_state.num=0
-if 'num1' not in session_state:
+if 'num1' not in st.session_state:
  st.session_state.num1=random.randint(1,20)
  st.session_state.num2=random.randint(1,20)
  st.session_state.sign=random.choice(['+','-','*','/'])
