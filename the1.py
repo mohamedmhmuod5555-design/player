50ac37f5a5ca17133512c35141d010d1b56a53a9
mohamedmhmuod5555-design <mohamedmhmuod5555@gmail.com>
2026-07-25 17:28:44 -0700

    Update the1.py

--- the1.py
+++ the1.py
@@ -31,7 +31,7 @@ if number==sc:
      del st.session_state.num2
      del st.session_state.sign
      st.rerun()
-    else:
+     else:
       st.error(f"اجابتك خاطئة! الإجابة الصحيحة كانت: {sc}") 
       st.session_state.num=0
       del st.session_state.num1 
