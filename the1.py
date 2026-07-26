f118881d39338757ada843a94f86904cc7244695
mohamedmhmuod5555-design <mohamedmhmuod5555@gmail.com>
2026-07-25 17:13:29 -0700

    Update the1.py

--- the1.py
+++ the1.py
@@ -30,7 +30,7 @@ if st.button("تاكيد الاجابه "):
     st.success("اجابتك صحيحه ! لقد احسنت  ")
     st.session_state.num+=1
   else:
-   st.error("اجابتك خاطءه ! الاجابه الصحيحه كانت  ",st.session_state.sc) 
+   st.error(f"اجابتك خاطئة! الإجابة الصحيحة كانت: {sc}") 
    st.session_state.num=0
 if st.button(" السؤال التالي "):
  del st.session_state.num1 
