f19cdddadaf05f5f7ca3a7f5700988ca3acf0292
mohamedmhmuod5555-design <mohamedmhmuod5555@gmail.com>
2026-07-25 17:27:37 -0700

    Update the1.py

--- the1.py
+++ the1.py
@@ -24,8 +24,6 @@ if sign=='/':
 st.title("أهلا بك في لعبتي ")
 st.write(num1,sign,num2)
 number=st.number_input("ادخل النتيجه ")
-if st.button("تاكيد الاجابه "):
-  st.session_state.count+=1
  if number==sc:
      st.success("اجابتك صحيحه ! لقد احسنت  ")
      st.session_state.num+=1 
@@ -33,7 +31,7 @@ if st.button("تاكيد الاجابه "):
      del st.session_state.num2
      del st.session_state.sign
      st.rerun()
-    else:
+   else:
      st.error(f"اجابتك خاطئة! الإجابة الصحيحة كانت: {sc}") 
      st.session_state.num=0
      del st.session_state.num1 
