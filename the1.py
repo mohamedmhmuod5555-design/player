ea938e7c2b61a881bef6ecd0a077852baed96abb
mohamedmhmuod5555-design <mohamedmhmuod5555@gmail.com>
2026-07-25 17:15:47 -0700

    Update the1.py

--- the1.py
+++ the1.py
@@ -29,12 +29,15 @@ if st.button("تاكيد الاجابه "):
   if number==sc:
     st.success("اجابتك صحيحه ! لقد احسنت  ")
     st.session_state.num+=1
+     del st.session_state.num1 
+     del st.session_state.num2
+     del st.session_state.sign
+     st.rerun()
   else:
    st.error(f"اجابتك خاطئة! الإجابة الصحيحة كانت: {sc}") 
    st.session_state.num=0
-if st.button(" السؤال التالي "):
- del st.session_state.num1 
- del st.session_state.num2
- del st.session_state.sign
- st.rerun()
+   del st.session_state.num1 
+   del st.session_state.num2
+   del st.session_state.sign
+   st.rerun()
 st.write("your points are " ,st.session_state.num,"from",st.session_state.count,"Questions")
